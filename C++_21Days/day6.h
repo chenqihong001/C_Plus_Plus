55544d8f6ce9ccc6df86d75b4c9d70d174c2431d
chen <2386104975@qq.com>
2023-12-02 23:58:38 +0800

    新的开始

---
+++ C++_21Days/day6.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+#include <iostream>
+void Day6()
+{
+	//std::bitset<8> myBitset; bitset类型，方便表示二进制
+	//my
+}
---
+++ C++_21Days/main.cpp
@@ -0,0 +1,23 @@
+﻿#include <bitset>
+#include <iostream>
+
+int main() {
+    // 创建一个包含8位的bitset
+    std::bitset<8> myBitset;
+
+    // 设置位
+    myBitset.set(1);
+    myBitset.set(3);
+    myBitset.set(5);
+
+    // 输出二进制表示
+    std::cout << "Binary representation: " << myBitset << std::endl;
+
+    // 获取位的值
+    std::cout << "Bit at position 2: " << myBitset.test(2) << std::endl;
+
+    // 以整数形式输出
+    std::cout << "Integer representation: " << myBitset.to_ulong() << std::endl;
+
+    return 0;
+}
