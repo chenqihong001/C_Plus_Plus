1b7c3730a03172e516676631b7b7fbd3a31746f1
chen <2386104975@qq.com>
2023-12-12 11:18:07 +0800

    c++_day12更新了35%

--- C++_21Days/main.cpp
+++ C++_21Days/main.cpp
@@ -3,12 +3,13 @@
 #include "day9.h"
 #include "day10.h"
 #include "day11.h"
+#include "day12.h"
 #include <memory>
 int main()
 {
-	Base11* arr[10]{};
-	std::unique_ptr<Derived11>d1(new Derived11());
-	arr[0] = d1;
-	Base11* brr = new Base11[10]{};
-
+	Date d(21, 3, 2004);
+	std::cout << (const char*)d<<"\n";//使用强制类型转换来确认转换意图
+	//Date d1 = { 11,5,2003 };隐式类型转换
+	std::string str(static_cast<const char*>(d));
+	std::cout << str;
 }
